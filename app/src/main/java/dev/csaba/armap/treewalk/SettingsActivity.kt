be75dfde71eda6545fb55a3e7491259ef1361ab6
MrCsabaToth <csaba.toth.us@gmail.com>
2023-02-06 22:37:53 -0800

    Display back button over the preferences screen

--- app/src/main/java/dev/csaba/armap/treewalk/SettingsActivity.kt
+++ app/src/main/java/dev/csaba/armap/treewalk/SettingsActivity.kt
@@ -1,6 +1,7 @@
 package dev.csaba.armap.treewalk
 
 import android.os.Bundle
+import android.view.View
 import androidx.appcompat.app.AppCompatActivity
 
 class SettingsActivity : AppCompatActivity() {
@@ -11,5 +12,18 @@ class SettingsActivity : AppCompatActivity() {
             .beginTransaction()
             .replace(R.id.settings_container, SettingsFragment())
             .commit()
+
+        supportActionBar?.setDisplayHomeAsUpEnabled(true)
+    }
+
+    override fun onWindowFocusChanged(hasFocus: Boolean) {
+        super.onWindowFocusChanged(hasFocus)
+
+        if (hasFocus) {
+            window.decorView.systemUiVisibility = (
+                View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+                or View.SYSTEM_UI_FLAG_VISIBLE
+                or View.STATUS_BAR_VISIBLE)
+        }
     }
 }
